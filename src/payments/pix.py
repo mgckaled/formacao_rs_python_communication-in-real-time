97cab399206cb001bfeb8ff6a819cbb8f20073b8
mgckaled <marcelgckaled@gmail.com>
2024-04-24 14:39:40 -0300

    feat: create pix payment

---
+++ src/payments/pix.py
@@ -0,0 +1,27 @@
+import uuid
+
+import qrcode
+
+
+class Pix:
+
+    def __init__(self) -> None:
+        pass
+
+    def create_payment(self):
+        # criar pagamento na instituição financeira
+        bank_payment_id = uuid.uuid4()
+
+        # copia e cola
+        hash_payment = f'hash_payment_{bank_payment_id}'
+
+        # qr code
+        img = qrcode.make(hash_payment)
+
+        # definir caminho das imagens e sua extensão
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {
+            "bank_payment_id":  bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}",
+        }
