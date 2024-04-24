1b5e87982c54c2a873b4749421d229b1a53997d1
mgckaled <marcelgckaled@gmail.com>
2024-04-24 04:22:47 -0300

    feat: implement create payment route

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, jsonify
+import os
+from datetime import datetime, timedelta
+
+from flask import Flask, jsonify, request
 
 from db_models.payment import Payment
 from repository.database import db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# Obtendo o caminho absoluto para a pasta src
+src_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Definindo o caminho para o banco de dados dentro da pasta src
+database_file = os.path.join(src_dir, "instance", "database.db")
+
+# Configurando o URI do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{database_file}"
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
@@ -13,7 +23,22 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
-    return jsonify({"message": "The payment has been created"})
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({"message": "invalid value"}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({
+        "message": "The payment has been created",
+        "payment": new_payment.to_dict(),
+    })
 
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
--- src/db_models/payment.py
+++ src/db_models/payment.py
@@ -2,10 +2,50 @@ from repository.database import db
 
 
 class Payment(db.Model):
-    # id, value, paid, bank_payment_id, qr_code, expiration_date
+    """
+    Represents a payment with the following attributes:
+    - id: Integer, primary key
+    - value: Float
+    - paid: Boolean, default is False
+    - bank_payment_id: Integer, nullable
+    - qr_code: String, nullable
+    - expiration_date: DateTime
+
+    Methods:
+    - to_dict(): Returns a dictionary representation of the Payment object.
+    """
+
+    __tablename__ = 'payment'
+
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float)
     paid = db.Column(db.Boolean, default=False)
     bank_payment_id = db.Column(db.Integer, nullable=True)
     qr_code = db.Column(db.String(100), nullable=True)
     expiration_date = db.Column(db.DateTime)
+
+    def __init__(self, value, expiration_date):
+        self.value = value
+        self.expiration_date = expiration_date
+
+    def to_dict(self):
+        """
+    Converts the Payment object into a dictionary representation.
+
+    Returns:
+        dict: A dictionary containing the following attributes of the Payment object:
+            - id (int): The primary key of the payment.
+            - value (float): The value of the payment.
+            - paid (bool): Indicates whether the payment is paid or not.
+            - bank_payment_id (int or None): The ID of the bank payment (if available).
+            - qr_code (str or None): The QR code associated with the payment (if available).
+            - expiration_date (datetime.datetime or None): The expiration date of the payment.
+    """
+        return {
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qr_code": self.qr_code,
+            "expiration_date": self.expiration_date,
+        }
